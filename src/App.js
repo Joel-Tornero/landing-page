c929c9ed463715eeb148ece20b86f2b0d9a4121a
Joel Tornero <joeltornero@Joels-MBP.lan>
2024-05-07 11:38:48 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Joel Torneros Portfolio Webpage</h1>
+        <p>Take a look at some of my best projects below, ranging from figma prototypes to coding projects, and aswell as team based design research  </p>
       </header>
+      <div className="gallery">
+        <div className="box">
+          <h2>Coding Projects</h2>
+        </div>
+        <div className="box">
+          <h2>Figma</h2>
+        </div>
+        <div className="box">
+          <h2>Research</h2>
+        </div>
+      </div>
+      <footer className="App-footer">
+        <p>UC San Diego  |  UX/UI Designer  |  tornerojoel@gmail.com  |  626-848-3349  </p>
+      </footer>
     </div>
   );
 }
 
-export default App;
+export default App
